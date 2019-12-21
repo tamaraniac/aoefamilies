50d655c6a6a35353bacc49a80f650a670f893904
Tamara Nelson-Fromm <tln2@illinois.edu>
2019-12-21 16:09:27 -0600

    more setup

--- familytree.js
+++ familytree.js
@@ -6,20 +6,29 @@ $(function() {
 })
 
 var visualize = function(data) {
-  // log data for debugging
-  console.log(data);
-
   // boilerplate setup
   var margin = { top: 50, right: 50, bottom: 50, left: 50 },
      width = 960 - margin.left - margin.right,
      height = 1080 - margin.top - margin.bottom;
 
-  var vis = d3.select('#chart')
+  var svg = d3.select('#chart')
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .style("width", width + margin.left + margin.right)
-  .style("height", height + margin.top + margin.bottom)
-  .append("g")
+  .style("height", height + margin.top + margin.bottom);
+
+  var g = svg.append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // create hierachy structure from data
+  var tree_data = d3.stratify()
+    .id(function(d) { return d.Little; })
+    .parentId(function(d) { return d.Big; })
+    (data);
+
+  // create tree layout
+  var root = d3.tree(tree_data);
+
+  
 }
